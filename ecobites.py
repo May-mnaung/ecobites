99f0c8c4fffdf1ea48c48fc5384a1c6d35d7c00c
May-mnaung <mmnaung.2022@scis.smu.edu.sg>
2023-10-19 23:32:44 +0800

    Update ecobites.py

--- ecobites.py
+++ ecobites.py
@@ -1,12 +1,9 @@
-from typing import Self
 from flask import Flask, jsonify, render_template, request, redirect, url_for
 import dao
 
 app = Flask(__name__)
 
-def get_data(data):
-     data['_id'] = str(data['_id'])
-     return data
+
 
 
 @app.route('/', methods=['GET'])
@@ -23,7 +20,6 @@ def restaurant():
     restaurant Routing 
     """
     restaurants = dao.fetch_Restaurants()
-    #restaurants = dao.fetch_Restaurants()
     return render_template("/restaurants.html", restaurantsinfo=restaurants)
 
 @app.route('/contact', methods = ['GET'])
@@ -33,12 +29,6 @@ def contact():
     """
     return render_template("/contact.html")
 
-
-
-# @app.route('/restaurant', methods = ['GET'] )
-# def get_restaurants():
-#     restaurants = dao.fetch_Restaurants()
-#     return jsonify([get_data(restaurant) for restaurant in restaurants])
 
 if __name__ == "__main__":
     app.run(debug= True)
