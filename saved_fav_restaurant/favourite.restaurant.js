d5b8dba9b7fb4b8133893709da8db0e49b69b5de
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-10-21 20:21:40 +0800

    /

--- saved_fav_restaurant/favourite.restaurant.js
+++ saved_fav_restaurant/favourite.restaurant.js
@@ -1,27 +1,43 @@
-function click_liked_button() {
+// to connecting to MongoDB server and update a document of its values or to retrieve values
 
-    console.log("**** [START]  *****")
+//to update MongoDB values using Node.js with the MongoDB driver 
+//(Mongoose or MongoDB native driver)
 
-   
-
-    // 1) Complete the endpoint URL with gender appended as a parameter
-    let api_endpoint_url = ""
-
-    // 2) Use Axios to call API asynchronously
-    axios.get(api_endpoint_url)
-    .then(response => {
-
-        // SUCCESS
-        // Got an API response
+// we use mongoDB native driver to  interact with MongoDB form a Node.js application.
 
 
-    })
-    .catch(error => {
 
-        // ERROR
-        // Something went wrong
-        console.log(error.message)
-    })
 
-    console.log("**** [END] *****")
+//Use the following code to connect to your MongoDB server and update a document:
+
+const { MongoClient } = require('mongodb');
+
+const uri = 'mongodb://localhost:27017/your-database-name';
+const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+async function updateDocument() {
+  try {
+    await client.connect();
+
+    const collection = client.db('your-database-name').collection('your-collection-name');
+    
+    const filter = { /* your filter criteria */ };
+    const update = { $set: { /* updated values */ } };
+
+    const result = await collection.updateOne(filter, update);
+
+    console.log(`${result.modifiedCount} document(s) updated`);
+  } finally {
+    await client.close();
+  }
 }
+
+updateDocument().catch(console.error);
+
+
+
+//Just like in the Node.js example, replace 'your-database-name', 
+//'your-collection-name', filter, and update with your specific details.
+
+//In both examples, you connect to the MongoDB server, 
+//specify the filter criteria to select the document(s) you want to update, and define the update operation using $set. The updateOne function in Node.js and update_one in Python are used to perform the update, and the result.modifiedCount or result.modified_count will give you the number of documents updated.
